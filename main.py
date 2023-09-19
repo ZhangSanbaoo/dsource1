a4d0c42105b68641de6edd50dec561b6b64b06a5
ZhangSanbaoo <melub.the.magician@gmail.com>
2023-09-19 15:38:41 +0200

    changed main

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def root():
     return "Hello from Space! 🚀"
+
+
+def hello_world():
+    prefix_google = """
+    <!-- Google tag(gtag.js) - ->
+    <script async src = "https://www.googletagmanager.com/gtag/js?id=G-MEC1VCR28C" > </script >
+    <script >
+    window.dataLayer = window.dataLayer | | []
+    function gtag(){dataLayer.push(arguments)
+                    }
+    gtag('js', new Date())
+
+    gtag('config', 'G-MEC1VCR28C')
+    </script >
+    """
+    return prefix_google + "Hello World"
